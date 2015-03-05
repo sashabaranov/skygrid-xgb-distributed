743c5331db740fd0633cc8b0ce05d8294c2914cf
root <root@qbic.(none)>
2015-03-06 02:38:06 +0300

    producer.sh changed

--- Consul/producer.sh
+++ Consul/producer.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-consumer=$(curl localhost:8500/v1/catalog/nodes | grep "node2" | awk 'BEGIN { FS = "[\[{:,}\]\"]+" } { print $5 }')
+consumer="$(ifconfig eth0 | awk -F ' *|:' '/inet addr/{print $4}')"
 echo will send to $consumer
-echo hello | nc $consumer 1027
 echo send hello
