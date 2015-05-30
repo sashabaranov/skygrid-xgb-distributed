37f82fd9bdf0a898aa374fb534db062251700ce8
Кочетов Николай Анатольевич <kochetovnicolai@gmail.com>
2015-05-30 21:12:13 +0300

    docs added

--- multi-node-example/nfs-docker/start.sh
+++ multi-node-example/nfs-docker/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo docker run -t -i -p 7000:2049  --privileged=true --rm=true   kochetovnicolai/nfs-etcd-server
+sudo docker run -t -i -p 7004:2049  --privileged=true --rm=true   kochetovnicolai/nfs-etcd-server
 
